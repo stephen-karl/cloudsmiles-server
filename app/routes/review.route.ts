d41ea6bd568f731239bea9176842baf3e737c58b
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-17 23:57:57 +0800

    Delete app directory

--- app/routes/review.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createReview } from "../controllers/review.controller";
-
-const router = Router()
-router.post('/create-review/', createReview)
-
-export default router 
