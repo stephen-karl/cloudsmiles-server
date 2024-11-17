c1a3742248d2bc2a58aa502fa78e7dc8a5e43289
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-17 23:55:28 +0800

    Delete app directory

--- app/helpers/cloudinary/extractor.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const extractCloudinaryPublicId = (url: string): string => {
-  const splitUrl = url.split('/');
-  const publicId = splitUrl[splitUrl.length - 1].split('.')[0];
-  return publicId;
-}
-
