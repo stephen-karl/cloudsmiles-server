13b5f3e1a332f4ad6b8d5934c26aeae161a4fe61
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-20 14:24:20 +0800

    Update mongo.config.ts

--- app/configs/mongo.config.ts
+++ app/configs/mongo.config.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-<<<<<<< HEAD
 dotenv.config();
 
 const connectToMongo = async () => {
@@ -11,13 +10,6 @@ const connectToMongo = async () => {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-=======
-const connectToMongo = async () => {
-  const dbUrl = process.env.MONGODB_DATABASE_URL + '/cloudsmiles';
-  await mongoose.connect(dbUrl)
-    .then(() => console.log('Successfully connected to MongoDB'))
-    .catch((error: Error) => console.error('MongoDB connection error:', error));
->>>>>>> c0c30f4d0a2b1d248cf5135918e66b1609abf748
-};
+}
 
 export default connectToMongo;
