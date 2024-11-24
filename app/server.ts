96b7ba7c6af2ba113d16a98580a945c01196027c
eternalramenz <147462463+eternalramenz@users.noreply.github.com>
2024-11-24 17:01:45 +0800

    Update server.ts

--- app/server.ts
+++ app/server.ts
@@ -43,7 +43,7 @@ async function startServer() {
     app.use(cookieParser());
 
     app.use(cors({
-      origin: process.env.FRONTEND_URL,  // Set the exact origin
+      origin: 'http://localhost:5173,  // Set the exact origin
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,  // Allow credentials (cookies, authorization headers)
     }));
